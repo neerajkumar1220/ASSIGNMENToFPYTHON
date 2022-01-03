11c5e660b36701498653dea82946612ad9325912
neerajkumar1220 <65033698+neerajkumar1220@users.noreply.github.com>
2022-01-04 00:22:14 +0500

    Add files via upload

---
+++ Assignment#1.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+
+print('Twinkle, Twinkle little star')
+print('        How i wonder what you are|')
+print('                Up above the world so high')
+print('                Like a diamond in the sky')
+print('Twinkle, Twinkle little star')
+print('        How i wonder what you are|')
+
+
+# In[5]:
+
+
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+
+# In[6]:
+
+
+from datetime import date
+
+today = date.today()
+print("Today's date:", today)
+
+
+# In[7]:
+
+
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
+
+
+# In[9]:
+
+
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Hello MR/MRS  " + lname + " " + fname)
+
+
+# In[16]:
+
+
+A= int(input("ENTER FIRST NUMBER : "))
+B = int(input("ENTER 2ND NUMBER: "))
+C=A+B
+print("The two numbers sum is ",C)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
